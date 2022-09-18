adf09846c170a69c0fc8a4b3f610269f0b324edf
Deepankar <deepankarpandey5843@gmail.com>
2022-09-18 21:11:18 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+ 
+const mongoose = require("mongoose");
+
+
+
+function connectDB() {
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+};
+
+mongoose
+.connect(process.env.MONGO_CONNECTION_URL, connectionParams)
+.then(() => {
+    console.info("Connected to the DB");
+})
+.catch((err) => {
+    console.log("Error:", err);
+})
+}
+module.exports = connectDB;
