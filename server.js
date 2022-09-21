906f474bb0f718e19e2faced5bb1164dae94b5b9
Deepankar <deepankarpandey5843@gmail.com>
2022-09-21 17:51:19 +0530

    add cors

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 
 const connectDB = require('./config/db');
 connectDB();
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
