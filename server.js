222e47debe75903c663dbe00af0bbe3895e32aca
Deepankar <deepankarpandey5843@gmail.com>
2022-09-22 00:31:24 +0530

    update code

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 connectDB();
 
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOptions));
-app.use(express.static('public'));
+
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
